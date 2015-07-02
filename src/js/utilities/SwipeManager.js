3eb0e60c2e599a251b6e09159ed1de88840986d0
Justin Oblak <jroblak@gmail.com>
2015-07-02 15:40:40 -0400

    add inelegant swipe cooldown

--- src/js/utilities/SwipeManager.js
+++ src/js/utilities/SwipeManager.js
@@ -2,6 +2,8 @@ var SwipeManager = function(game, bounds, callback) {
   Phaser.Plugin.call(this, game);
   this.game = game;
   this.bounds = bounds;
+  this.onCooldown = false;
+  this.cooldown = 0;
   this.onSwipe = callback;
 };
 
@@ -16,14 +18,25 @@ SwipeManager.prototype.update = function() {
   var distance = Phaser.Point.distance(start, end);
   var duration = this.game.input.activePointer.duration;
 
-  if (duration > 0) {
+  if (duration > 0 && !this.onCooldown) {
     if (distance > SwipeManager.SWIPE_DIST && duration < SwipeManager.SWIPE_TIME) {
       this.onSwipe();
+      this.onCooldown = true;
+    }
+  }
+
+  if (this.onCooldown) {
+    this.cooldown += 1;
+
+    if (this.cooldown >= SwipeManager.SWIPE_COOLDOWN) {
+      this.cooldown = 0;
+      this.onCooldown = false;
     }
   }
 };
 
 SwipeManager.SWIPE_TIME = 250;
 SwipeManager.SWIPE_DIST = 100;
+SwipeManager.SWIPE_COOLDOWN = 25;
 
 module.exports = SwipeManager;
