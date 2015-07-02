14768f22e1bb96b5e18fd98a80cdd7800bca0a76
Justin Oblak <jroblak@gmail.com>
2015-07-02 16:42:45 -0400

    thinking out loud

--- src/js/entities/CinderProfile.js
+++ src/js/entities/CinderProfile.js
@@ -1,10 +1,14 @@
 var CinderProfile = function(game) {
   this.image = null;
   this.profileText = null;
+  this.selectionResult = null;
   this.generateProfile();
 };
 
 CinderProfile.prototype.generateProfile = function() {
+  // Generate or randomly pick already generated image
+  // Generate or randomly pick already generated profile text
+  // Generate result of R/L swipes for this profile
 };
 
 module.exports = CinderProfile;
